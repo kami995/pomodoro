e920aebff3e4036ba3fd795ea5eb179fde150f62
Kami <mihailopetrovic995@gmail.com>
2020-04-18 23:34:50 +0200

    Updates

---
+++ precache-manifest.d74c67ee082361283f59b86262544446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dfd87c2141525965e1816ed6c563940",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "24a54c8411458b6925bf",
+    "url": "/pomodoro/static/css/main.07d52b91.chunk.css"
+  },
+  {
+    "revision": "e1c15037fa51e80f041e",
+    "url": "/pomodoro/static/js/2.e7829b4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.e7829b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a54c8411458b6925bf",
+    "url": "/pomodoro/static/js/main.170a2b42.chunk.js"
+  },
+  {
+    "revision": "5180613559922e17f966",
+    "url": "/pomodoro/static/js/runtime-main.64058674.js"
+  }
+]);
